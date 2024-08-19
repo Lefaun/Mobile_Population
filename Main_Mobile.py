0ba173178812318af2a30c3b5af0babc14656416
Lefaun <118660975+Lefaun@users.noreply.github.com>
2024-08-19 11:10:44 +0100

    Update Main_Mobile.py

--- Main_Mobile.py
+++ Main_Mobile.py
@@ -16,68 +16,13 @@ import statsmodels.api as sm
 import numpy as np
 import plotly.express as px # interactive charts 
 
-
-#######################
-# Page configuration
-
 st.set_page_config(
-    page_title="PT Population Dashboard",
+    page_title="Portugal Population Dashboard",
     page_icon="🏂",
     layout="wide",
     initial_sidebar_state="expanded")
 
 alt.themes.enable("dark")
-
-#######################
-# CSS styling
-st.markdown("""
-<style>
-
-[data-testid="block-container"] {
-    padding-left: 2rem;
-    padding-right: 2rem;
-    padding-top: 1rem;
-    padding-bottom: 0rem;
-    margin-bottom: -7rem;
-}
-
-[data-testid="stVerticalBlock"] {
-    padding-left: 0rem;
-    padding-right: 0rem;
-}
-
-[data-testid="stMetric"] {
-    background-color: #393939;
-    text-align: center;
-    padding: 15px 0;
-}
-
-[data-testid="stMetricLabel"] {
-  display: flex;
-  justify-content: center;
-  align-items: center;
-}
-
-[data-testid="stMetricDeltaIcon-Up"] {
-    position: relative;
-    left: 38%;
-    -webkit-transform: translateX(-50%);
-    -ms-transform: translateX(-50%);
-    transform: translateX(-50%);
-}
-
-[data-testid="stMetricDeltaIcon-Down"] {
-    position: relative;
-    left: 38%;
-    -webkit-transform: translateX(-50%);
-    -ms-transform: translateX(-50%);
-    transform: translateX(-50%);
-}
-
-</style>
-""", unsafe_allow_html=True)
-
-alt.themes.enable("light")
 
 
 #######################
@@ -498,3 +443,7 @@ def perform_regression(time, population):
     X = sm.add_constant(time)
     model = sm.OLS(population, X).fit()
     return model
+
+# Configuração da interface do Streamlit
+#st.set_page_config(page_title="Simulação de População em Tempo Real", layout="wide")
+
